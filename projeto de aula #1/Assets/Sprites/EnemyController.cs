8f3359d98a15dbdcda922e637e229471d3e1c681
EthanHTDev <141367304+EthanHTDev@users.noreply.github.com>
2024-05-15 19:57:57 -0300

    update #11
    inimigo bugado

--- projeto de aula #1/Assets/Sprites/EnemyController.cs
+++ projeto de aula #1/Assets/Sprites/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -8,16 +9,65 @@ public class EnemyController : MonoBehaviour
     public Transform waypointA;
     public Transform waypointB;
     public float movimentSpeed = 2f;
-    
+    private Animator animator;
+    private bool isWalking = false;
+    private Rigidbody2D rb;
+    private Vector3 scale;
+    private Transform currentTarget;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        currentTarget = waypointA;
+        scale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget() 
+    {
+        Vector3 curTargetHorizontal = new Vector2(currentTarget.position.x, transform.position.y);
+        Vector2 direction = (curTargetHorizontal - transform.position).normalized;
+
+        transform.position += (Vector3)direction * movimentSpeed * Time.deltaTime;
+
+        if(Vector2.Distance(curTargetHorizontal, transform.position) <= 0.2f)
+        {
+            SwitchTarget();
+        }
+
+        UpdateAnimation();
+    }
+
+    private void SwitchTarget()
+    {
+        if(currentTarget == waypointA)
+        {
+            currentTarget = waypointB;
+            Flip();
+        }
+        else
+        {
+            currentTarget = waypointA;
+            transform.localScale = scale;
+        }
+    }
+
+    private void UpdateAnimation()
+    {
+        isWalking = (Vector2.Distance(transform.position, currentTarget.position) > 8.1f);
+        animator.SetBool("isWalking", isWalking);
+    }
+
+    private void Flip()
+    {
+        Vector3 FlippedScale = scale;
+        FlippedScale.x *= -1;
+        transform.localScale = FlippedScale;
     }
 }
